eb29ffb4c28cce9be44a94a1f716c8d638a77374
Guillaume Aubert <aubertg@cpan.org>
2012-09-05 23:05:40 -0400

    Added Audit::DBI->record().

--- lib/Audit/DBI.pm
+++ lib/Audit/DBI.pm
@@ -105,6 +105,146 @@ sub new
 }
 
 
+=head2 record()
+
+Record an audit event along with information on the context and data changed.
+
+	$audit->record(
+		event               => $event,
+		subject_type        => $subject_type,
+		subject_id          => $subject_id,
+		event_time          => $event_time,
+		diff                => [ $old_structure, $new_structure ],
+		search_data         => \%search_data,
+		information         => \%information,
+		affected_account_id => $account_id,
+		file                => $file,
+		line                => $line,
+	);
+
+Required:
+
+=over 4
+
+=item * event
+
+The type of action performed (48 characters maximum).
+
+=item * subject_type
+
+Normally, the singular form of the name of a table, such as "object" or
+"account" or "order".
+
+=item * subject_id
+
+If subject_type is a table, the corresponding record ID.
+
+=back
+
+Optional:
+
+=over 4
+
+=item * diff
+
+This automatically calculates the differences between the two data structures
+passed as values for this parameter, and makes a new structure recording those
+differences.
+
+=item * search_data
+
+A hashref of all the key/value pairs that we may want to be able search on later
+to find this type of event. You may pass either a scalar or an arrayref of
+multiple values for each key.
+
+=item * information
+
+Any other useful information (such as user input) to understand the context of
+this change.
+
+=item * account_affected
+
+The ID of the account to which the data affected during that event where linked
+to, if applicable.
+
+=item * event_time
+
+Unix timestamp of the time that the event occurred, the default being the
+current time.
+
+=item * file and line
+
+The filename and line number where the event occurred, the default being the
+immediate caller of Audit::DBI->record().
+
+=back
+
+Note: if you want to delay the insertion of audit events (to group them, for
+performance), subclass C<Audit::DBI> and add a custom C<_insert_event method()>.
+
+=cut
+
+sub record ## no critic (NamingConventions::ProhibitAmbiguousNames)
+{
+	my( $self, %args ) = @_;
+	my $limit_rate_timespan = delete( $args{'limit_rate_timespan'} );
+	my $limit_rate_unique_key = delete( $args{'limit_rate_unique_key'} );
+	my $dbh = $self->get_database_handle();
+	
+	# Check required parameters.
+	foreach my $arg ( qw( event subject_type subject_id ) )
+	{
+		next if defined( $args{ $arg } ) && $args{ $arg } ne '';
+		croak "The argument $arg must be specified.";
+	}
+	croak('The argument "limit_rate_timespan" must be a strictly positive integer.')
+		if defined $limit_rate_timespan && ( $limit_rate_timespan !~ /^\d+$/ || $limit_rate_timespan == 0 );
+	croak('The argument "limit_rate_unique_key" must be a string with length greater than zero.')
+		if defined $limit_rate_unique_key && length $limit_rate_unique_key == 0;
+	croak('Both "limit_rate_timespan" and "limit_rate_unique_key" must be defined.')
+		if defined $limit_rate_timespan != defined $limit_rate_unique_key;
+	
+	# Rate limiting.
+	if ( defined( $limit_rate_timespan ) )
+	{
+		if ( !defined( $self->_get_cache( key => $limit_rate_unique_key ) ) )
+		{
+			# Cache event.
+			$self->_set_cache(
+				key         => $limit_rate_unique_key,
+				value       => 1,
+				expire_time => time() + $limit_rate_timespan,
+			);
+		}
+		else
+		{
+			# No need to log audit event.
+			return 1;
+		}
+	}
+	
+	# Record the time (unless it was already passed in).
+	$args{'event_time'} ||= time();
+	
+	# Store the file and line of the caller, unless they were passed in.
+	if ( !defined( $args{'file'} ) || !defined( $args{'line'} ) )
+	{
+		my ( $file, $line ) = ( caller() )[1,2];
+		$file =~ s|.*/||;
+		$args{'file'} = $file
+			if !defined( $args{'file'} );
+		$args{'line'} = $line
+			if !defined( $args{'line'} );
+	}
+	
+	my $audit_event = $self->_insert_event( \%args );
+	
+	return defined( $audit_event )
+		? 1
+		: 0;
+}
+
+
 =head1 AUTHOR
 
 Guillaume Aubert, C<< <aubertg at cpan.org> >>.
