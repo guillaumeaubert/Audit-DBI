320e711c91db8e97a282883d4147c9cafe2b7bd2
Guillaume Aubert <aubertg@cpan.org>
2013-05-13 00:13:57 -0400

    Released version 1.8.0.

--- lib/Audit/DBI/Event.pm
+++ lib/Audit/DBI/Event.pm
@@ -16,11 +16,11 @@ Audit::DBI::Event - An event as logged by the Audit::DBI module.
 
 =head1 VERSION
 
-Version 1.7.3
+Version 1.8.0
 
 =cut
 
-our $VERSION = '1.7.3';
+our $VERSION = '1.8.0';
 
 
 =head1 SYNOPSIS
--- lib/Audit/DBI/Utils.pm
+++ lib/Audit/DBI/Utils.pm
@@ -16,11 +16,11 @@ Audit::DBI::Utils - Utilities for the Audit::DBI distribution.
 
 =head1 VERSION
 
-Version 1.7.3
+Version 1.8.0
 
 =cut
 
-our $VERSION = '1.7.3';
+our $VERSION = '1.8.0';
 
 
 =head1 SYNOPSIS
